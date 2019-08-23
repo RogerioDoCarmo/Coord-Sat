7b733ce2d9f2da21555670aa76bca5b599cd6785
Rogério do Carmo <rogerio.carmo02@gmail.com>
2019-08-23 10:42:50 -0300

    GNSS Sourcetable OKgit add .git add .!

--- src/main/java/NTRIP/Ntrip_Main.java
+++ src/main/java/NTRIP/Ntrip_Main.java
@@ -5,12 +5,14 @@
  */
 package NTRIP;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.Socket;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -43,8 +45,8 @@ public class Ntrip_Main {
             
                 client = new Socket(server,port);
 
-                client.setKeepAlive(true);
-                client.setSoLinger(false, 5);
+//                client.setKeepAlive(false);
+//                client.setSoLinger(false, 5);
                 client.setSoTimeout(60000);
 
                 input = client.getInputStream();
@@ -53,6 +55,8 @@ public class Ntrip_Main {
                 output = client.getOutputStream();
                 dataOut = new DataOutputStream(output);
 
+//                server = "78.46.41.26";
+                
                 String request = "GET /  HTTP/1.1\r\n"
                                + "Host: " +server+ "\r\n"
                                + "Ntrip-Version: Ntrip/1.0\r\n"
@@ -62,7 +66,16 @@ public class Ntrip_Main {
                                + "Accept-Language: pt-BR,en,*\r\n"
                                + "Connection: close";
 //                               + "\r\n";
-
+                request = "GET / HTTP/1.1\r\n"
+                        + "Host: 78.46.41.26:2101\r\n"
+                        + "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0\r\n"
+                        + "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
+                        + "Accept-Language: pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3\r\n"
+                        + "Accept-Encoding: gzip, deflate\r\n"
+                        + "Connection: keep-alive\r\n"
+                        + "Upgrade-Insecure-Requests: 1\r\n"
+                        + "\r\n";
+                                               
 
                 Logger.getLogger(Ntrip_Main.class.getName()).log(Level.INFO, "Requisição Criada!\n");
 
@@ -110,8 +123,7 @@ public class Ntrip_Main {
                 StringBuffer buff2 = new StringBuffer( );
                 buff2.append("\n");
 
-
-
+                
                 while (((dadosEntrada = dataInput.read())!= -1)) {
                     buff2.append( (char) dadosEntrada );
                 }
@@ -136,9 +148,41 @@ public class Ntrip_Main {
         
     }
     
+    public static void getSource2() throws IOException {
+        //Instantiate a new socket
+        Socket s = new Socket("products.igs-ip.net", 2101);
+
+        //Instantiates a new PrintWriter passing in the sockets output stream
+        String wtr = "";// = new PrintWriter(s.getOutputStream());
+
+        //Prints the request string to the output stream
+        wtr += "GET / HTTP/1.1\r\n"
+            + "Host: products.igs-ip.net\r\n"
+            + "Ntrip-Version: Ntrip/2.0\r\n"
+            + "User-Agent: NTRIP Coord_Sat/1.0\r\n"
+            + "\r\n";
+
+        s.getOutputStream().write(wtr.getBytes());
+        
+        s.getOutputStream().flush();
+        
+        //Creates a BufferedReader that contains the server response
+        BufferedReader bufRead = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        String outStr;
+
+        //Prints each line of the response 
+        while((outStr = bufRead.readLine()) != null){
+            System.out.println(outStr);
+        }
+
+        //Closes out buffer and writer
+        bufRead.close();
+//        wtr.close();
+    }
+    
     public static void main (String[] args) {
         try {
-            getSourceTable();
+            getSource2();
         } catch (IOException ex) {
             Logger.getLogger(Ntrip_Main.class.getName()).log(Level.SEVERE, null, ex);
         }
